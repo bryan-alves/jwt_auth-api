cb128f5cec476b6e764c96c1f0eacdd425b81b8c
Bryan Alves <=>
2021-06-02 17:29:38 -0300

    Configuração do JWT e criação do token/login

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Hash;
+
+class AuthController extends Controller
+{
+
+    public function index(Request $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
