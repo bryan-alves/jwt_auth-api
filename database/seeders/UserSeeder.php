c7881d26ac0f2f50ced6aaa7907fe534ab72906b
Bryan Alves <=>
2021-06-02 17:38:46 -0300

    Seeder para criação do usuário padrão

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Hash;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Usuário de Teste',
+            'email' => 'test@test.com',
+            'password' => Hash::make('123'),
+        ]);
+    }
+}
